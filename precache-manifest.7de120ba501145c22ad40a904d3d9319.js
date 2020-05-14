b42474304090e1c6c8d1109014e000ee8d54bdd8
endayusma <enda.nur.a.y@mail.ugm.ac.id>
2020-05-14 11:50:54 +0700

    Updates

---
+++ precache-manifest.7de120ba501145c22ad40a904d3d9319.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9f773ee49cdd60f0c434ed256db2292",
+    "url": "/iai_novelist/index.html"
+  },
+  {
+    "revision": "e6bb5b85b897fe88e6c2",
+    "url": "/iai_novelist/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "dbdedafaa4ffebd69792",
+    "url": "/iai_novelist/static/css/main.282b32d5.chunk.css"
+  },
+  {
+    "revision": "e6bb5b85b897fe88e6c2",
+    "url": "/iai_novelist/static/js/2.630010d5.chunk.js"
+  },
+  {
+    "revision": "494ff15427fc00bc6d2909d429ff11b9",
+    "url": "/iai_novelist/static/js/2.630010d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbdedafaa4ffebd69792",
+    "url": "/iai_novelist/static/js/main.2450e21a.chunk.js"
+  },
+  {
+    "revision": "b9e62889d04f0367196d",
+    "url": "/iai_novelist/static/js/runtime-main.55c3fdb3.js"
+  }
+]);
